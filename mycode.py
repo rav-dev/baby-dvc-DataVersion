84c02ab13098764b657e7828d3332c4e6131401e
rav-dev <agg.ravish@gmail.com>
2026-07-30 13:17:34 +0200

    Second version of data saved

--- mycode.py
+++ mycode.py
@@ -12,6 +12,14 @@ data = {
 
 df = pd.DataFrame(data)
 
+
+
+
+# Adding new row to the df for data version 2 
+
+new_row_loc = {'Name':'GF1', 'Age':20, 'City': 'Cty1'}
+df.loc[len(df.index)] = new_row_loc
+
 #ensures that the data dir exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok = True) #exist_ok if the folder already exists with the same name then it will not create a new folder with the same name
@@ -21,5 +29,4 @@ file_path = os.path.join(data_dir,'sample_data.csv')
 
 #save the DataFrame to a csv file, including column names
 df.to_csv(file_path, index = False)
-
 print(f"CSV file saved to {file_path}")
