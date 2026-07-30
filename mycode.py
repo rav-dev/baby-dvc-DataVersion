5cf884c31bb0e0c2ef7ee41b56067fc4f07a62d8
rav-dev <agg.ravish@gmail.com>
2026-07-30 13:23:38 +0200

    Third version of data saved

--- mycode.py
+++ mycode.py
@@ -20,6 +20,13 @@ df = pd.DataFrame(data)
 new_row_loc = {'Name':'GF1', 'Age':20, 'City': 'Cty1'}
 df.loc[len(df.index)] = new_row_loc
 
+
+# Adding new row to the df for data version 3 
+new_row_loc2 = {'Name':'GF1', 'Age':30, 'City': 'Cty2'}
+df.loc[len(df.index)] = new_row_loc2
+
+
+
 #ensures that the data dir exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok = True) #exist_ok if the folder already exists with the same name then it will not create a new folder with the same name
